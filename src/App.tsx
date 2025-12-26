223732d3117e2c22927d953f29e018afc60ead3e
uknownymous-rn <mdamirkamal03@gmail.com>
2025-12-26 14:30:26 +0530

    base router name change

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/edu-boost-path">
+        <BrowserRouter basename="/futureforge-nexus">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/about" element={<About />} />
