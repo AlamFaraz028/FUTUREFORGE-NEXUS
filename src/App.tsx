034c8eeaeeead4f524f74a84f922ea46d9b3c46f
uknownymous-rn <mdamirkamal03@gmail.com>
2025-12-26 11:41:09 +0530

    Initial commit for FutureForge Nexus

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/edu-boost-path">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/about" element={<About />} />
